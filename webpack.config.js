1cde311d0a704727f62cdec1bed974359ea0427c
Ian Paterson <ianhpaterson@gmail.com>
2022-09-22 20:24:34 -0400

    Clean up webpack config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist'),
+    path: path.resolve(__dirname, './dist'),
     clean: true,
   },
   resolve: {
